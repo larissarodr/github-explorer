44c698ca4a278a8f2c80d13fb99b877aa8ef4592
Larissa <larissa_rodr@hotmail.com>
2020-06-15 14:42:39 -0500

    Changes to deploy website in gh-pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
-  <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/repository/:repository+" component={Repository} />
-  </Switch>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repository/:repository+" component={Repository} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
